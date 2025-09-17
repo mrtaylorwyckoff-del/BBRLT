5bd947e56433be912709f42af4e43aa36a849212
MUKILTEO\1726619 <mrtaylorwyckoff@gmail.com>
2025-09-17 12:44:13 -0700

    Added full movement to sprite and positioned near start of map.

--- Big Bean Reasonably Large Tower/Assets/movement.cs
+++ Big Bean Reasonably Large Tower/Assets/movement.cs
@@ -1,9 +1,15 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Rendering;
 
 public class Movement : MonoBehaviour
 {
-    public float speed = 5f;
-    public float jumpForce = 5f;
+    public float speed;
+    public float jumpHeight;
+
+    public Rigidbody2D rb2d;
+
+    private float _movement;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -14,6 +20,16 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        rb2d.linearVelocityX = _movement;
+    }
+
+    public void Move(InputAction.CallbackContext ctx)
+    {
+        _movement = ctx.ReadValue<Vector2>().x;
+    }
+
+    public void Jump(InputAction.CallbackContext ctx)
+    {
+        rb2d.linearVelocityY = jumpHeight;
     }
 }
