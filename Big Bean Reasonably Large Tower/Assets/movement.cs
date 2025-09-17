cc62c3789efde61ecc566b2229f849ca0e43841b
MUKILTEO\1726619 <mrtaylorwyckoff@gmail.com>
2025-09-17 12:01:05 -0700

    Added very basic scripts to King Can sprite, will proceed to tweak in the near future.

--- Big Bean Reasonably Large Tower/Assets/movement.cs
+++ Big Bean Reasonably Large Tower/Assets/movement.cs
@@ -2,6 +2,9 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    public float speed = 5f;
+    public float jumpForce = 5f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
