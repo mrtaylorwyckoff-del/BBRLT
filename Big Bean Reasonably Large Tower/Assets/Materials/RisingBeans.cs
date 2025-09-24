45a34d08d68174b7bd74799b0284fac78ba4f492
MUKILTEO\1726619 <mrtaylorwyckoff@gmail.com>
2025-09-24 11:59:59 -0700

    Beans rise and do 1 damage haha (also you can't walk though them so don't die in the playthrough ;))

--- Big Bean Reasonably Large Tower/Assets/Materials/RisingBeans.cs
+++ Big Bean Reasonably Large Tower/Assets/Materials/RisingBeans.cs
@@ -3,10 +3,21 @@ using UnityEngine;
 public class RiseObject : MonoBehaviour
 {
     public float riseSpeed = 1f; // Speed at which the object rises
+    private object collision;
+    public PlayerHealth playerHealth;
+    public int damageAmount = 1;
 
     void Update()
     {
         // Move the object upwards along the Y-axis
         transform.Translate(Vector3.up * riseSpeed * Time.deltaTime);
+    }
+         private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            playerHealth.TakeDamage(1);
+        }
+
     }
 }
